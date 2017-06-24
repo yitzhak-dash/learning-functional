858d37e6960cee0368092c5aba95dc11a541ab81
Yitzhak Dashevsky <yitzhak@razorgrip.com>
2017-06-25 01:30:03 +0300

    add examples for curring

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # learning-functional
 `npm run play` - compile es6 and run it.
+
+# Terms:
+* **Currying**: it's a process of converting a function wth a `n` arguments into a *nested* unary function.
--- functional-playground/play.js
+++ functional-playground/play.js
@@ -1,21 +1,27 @@
-import * as lib from '../lib/es6-functional';
+import * as lib from "../lib/es6-functional";
+import * as _ from "lodash";
 
 const print = (arg) => console.log(arg);
-const ___ = () => console.log(`************************************************************`);
 
-var array = [1,2,3];
-lib.foreach(array,(data) => console.log(data)); //refereing to imported forEach
+const ___ = (mes = "") => {
+    const finalMessageLength = 70;
+    const resultMessage = _.pad(mes.toUpperCase(), finalMessageLength, "*");
+    console.log(`\n${resultMessage}`);
+};
 
-const obj = {id:123, name: "moshe"};
-lib.foreachObj(obj,(k,v) => console.log(`${k} -> ${v}`));
+var array = [1, 2, 3];
+lib.foreach(array, (data) => console.log(data)); //refereing to imported forEach
 
-print(lib.every([NaN,NaN,NaN],isNaN));
-print(lib.every([NaN, NaN, 4,NaN], isNaN));
+const obj = {id: 123, name: "moshe"};
+lib.foreachObj(obj, (k, v) => console.log(`${k} -> ${v}`));
+
+print(lib.every([NaN, NaN, NaN], isNaN));
+print(lib.every([NaN, NaN, 4, NaN], isNaN));
 
 var people = [
     {firstname: "aaFirstName", lastname: "cclastName"},
     {firstname: "ccFirstName", lastname: "aalastName"},
-    {firstname:"bbFirstName", lastname:"bblastName"}
+    {firstname: "bbFirstName", lastname: "bblastName"}
 ];
 
 print(people.sort(lib.sortBy("firstname")));
@@ -34,16 +40,15 @@ var closureFn = fn(5);
 closureFn();
 
 ___();
-lib.foreach([1,2,3],(a) =>
-    lib.tap(a)(() =>
-        {
+lib.foreach([1, 2, 3], (a) =>
+    lib.tap(a)(() => {
             console.log(a)
         }
     )
 );
 
 ___();
-const doPayment = lib.once(() => 'payment is done');
+const doPayment = lib.once(() => "payment is done");
 print(doPayment());
 print(doPayment());
 
@@ -61,6 +66,36 @@ print(fastFactorial(5));
 print(fastFactorial(3));
 
 ___();
-let testMem = lib.memorized((n) => {console.log('i am here');return n + 100});
+let testMem = lib.memorized((n) => {
+    console.log("i am here");
+    return n + 100
+});
 print(testMem(1));
 print(testMem(1));
+
+___("variadic");
+lib.variadic(1, 2, 3);
+
+___("carry");
+const add = (x, y) => x + y;
+const autoCurriedFn = lib.curry(add);
+print(autoCurriedFn(1)(2));
+
+___("Curring in Action");
+___("Finding number in Array Contents");
+const match = lib.curry((expr, str) => str.match(expr));
+const hasNumber = match(/[0-9]+/);
+const filter = lib.curry((predicate, arr) => arr.filter(predicate));
+const findNumberInArray = filter(hasNumber);
+print(findNumberInArray(['one', '456brrr', '1a2b3']));
+
+___("squaring an Array");
+const map = lib.curry((func, arr) => arr.map(func));
+const squareAll = map((x => x * x));
+print(squareAll([10000000000, 3, 4, 5, 6]));
+
+
+
+
+
+___('end');
